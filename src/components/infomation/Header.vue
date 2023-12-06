51383c28118142c9dff5b6ca6662c99c657b04d0
rockychen auo pc <rockychen@auo.com>
2023-12-06 13:37:10 +0800

    first commit

---
+++ src/components/infomation/Header.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps, computed } from "vue"
+
+const props = defineProps({
+  books: {
+    type: Array,
+    required: true,
+    default: [],
+  },
+})
+
+const bookCount = computed(() => {
+  return props.books.filter((book) => book.title.length > 0).length
+})
+</script>
+
+<template>
+  <h2 class="text-2xl" v-if="books.length == 0">no todo</h2>
+  <h2 class="text-2xl" v-else>now have {{ bookCount }} todo</h2>
+</template>
---
+++ src/pages/Home.vue
@@ -0,0 +1,65 @@
+<script setup>
+import { v4 as uuid } from "uuid"
+import { getBooks, saveBooks } from "@/auo-lib/storage"
+import { reactive, onBeforeMount } from "vue"
+import Header from "@/components/infomation/Header.vue"
+import BookItem from "@/components/books/Item.vue"
+import AddBookForm from "@/components/forms/AddBook.vue"
+
+const title = "TODO!"
+const subtitle = "simple and studid todo app"
+const books = reactive([])
+
+const removeBook = (id) => {
+  if (id) {
+    const bookIndex = books.findIndex((book) => book.id == id)
+
+    if (bookIndex >= 0) {
+      books.splice(bookIndex, 1)
+      saveBooks(books)
+    }
+  }
+}
+
+const addBook = (bookName) => {
+  if (bookName != "") {
+    const book = {
+      id: uuid(),
+      title: bookName,
+    }
+
+    books.unshift(book)
+
+    // save
+    saveBooks(books)
+  }
+}
+
+onBeforeMount(() => {
+  const data = getBooks()
+  if (data) {
+    books.push(...data)
+  }
+})
+</script>
+
+<template>
+  <h1 class="title">{{ title }}</h1>
+  <h3 class="subtitle">{{ subtitle }}</h3>
+
+  <AddBookForm @add-book="addBook" />
+
+  <div class="divider"></div>
+  <div>
+    <Header :books="books" />
+    <ul>
+      <BookItem @remove-book="removeBook" v-for="book in books" :book="book" />
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.title {
+  @apply text-slate-800 text-5xl;
+}
+</style>
